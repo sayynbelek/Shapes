0239e01611df50d7a4522ebb0f300a9395951b43
СайынБелек Алексеевич Кошкароол <sakoshkarool@ttit.local>
2018-02-10 10:08:05 +0700

    Add

---
+++ Shapes/Shapes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Fun with Polymorphism *****/n");
+            ThreeDCircle o = new ThreeDCircle();
+            o.Draw();
+            ((Circle)o).Draw();
+            Shape[] myShapes = {new Hexagon(), new Circle(), new Hexagon("Mick"),
+                new Circle("Beth"), new Hexagon("Linda") };
+            foreach (Shape s in myShapes)
+            {
+                s.Draw();
+            }
+            Console.ReadLine();
+        }
+    }
+}
